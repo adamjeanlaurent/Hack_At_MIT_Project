c8ea25d51ea26172b86a653fa40564ebaafb3fb0
Phidias Mendez <mendezphidias@gmail.com>
2020-09-19 14:57:22 -0400

    Server DB Bug Fix

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,18 +1,20 @@
 const MongoClient = require("mongodb").MongoClient;
 const mongoose = require('mongoose');
-require('dotenv').config();
+const { path } = require("./studentOrgSchema");
+require('dotenv').config({ path: '../../.env' })
 
 const dbname = process.env.DB_NAME;
 const username = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const uri = "mongodb+srv://" + username + ":" + password + "@cluster0.f1vj5.mongodb.net/" + dbname + "?retryWrites=true&w=majority";
 
+console.log(uri);
 
 try {
-  mongoose.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true });
-  console.log("Auth succeeded!")
+  mongoose.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+  console.log("Auth succeeded!");
 } catch (error) {
-  console.log("Auth failed.")
+  console.log("Auth failed.");
 }
 
 module.exports = mongoose;
--- backend/routes/studentOrgsController.js
+++ backend/routes/studentOrgsController.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('../db/db');
+
+require('dotenv').config({ path: '../../.env' })
+
 const StudentOrg = require('../models/studentOrg');
+
 const studentOrgSchema = require('../db/studentOrgSchema');
+
 
 // @route GET api/studentOrgs
 // @desc Gets all University Of Rhode Island Student Org Titles And Descriptions
 router.get('/', async (req, res) => {
-    console.log("Route started.");
-    const StudentOrgModel = mongoose.model('StudentOrg', studentOrgSchema);
-    console.log("Model created.");
+    await console.log("Route started.");
+    let StudentOrgModel = mongoose.model('StudentOrg', studentOrgSchema);
+    await console.log("Model created.");
 
-    StudentOrgModel.find(function(err, data){
+    await StudentOrgModel.find(function(err, data){
         if(err){
             console.log(err);
         } else {
