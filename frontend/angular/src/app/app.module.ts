40d4523dc183c206ff50f62f347e2940ea0b7538
LuisLeonardoPolanco <luis_leonardo@my.uri.edu>
2020-09-19 17:27:14 -0400

    Change of Directory
    Changed the directory of the category components folder, added a grid and organization component as well.

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategoryComponent } from './category/category.component';
+import { CategoryComponent } from './components/category/category.component';
+import { GridComponent } from './components/grid/grid.component';
+import { OrganizationComponent } from './components/organization/organization.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryComponent
+    CategoryComponent,
+    GridComponent,
+    OrganizationComponent
   ],
   imports: [
     BrowserModule,
